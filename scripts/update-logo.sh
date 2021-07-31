152ec8a1a1eb69d3f2f687c9b5add6fa34f29640
Nils Knappmeier <npm@knappi.org>
2021-07-31 14:04:27 +0200

    update logos

--- scripts/update-logo.sh
+++ scripts/update-logo.sh
@@ -8,3 +8,6 @@ convert -background transparent src/assets/logo.svg -resize 30x30 src/assets/log
 convert -background transparent src/assets/logo.svg -resize 192x192 public/logo192.png
 convert -background transparent src/assets/logo.svg -resize 512x512 public/logo512.png
 convert -background transparent src/assets/logo.svg -resize 16x16 public/favicon.ico
+
+git add src/assets/logo.png public/logo192.png public/logo512.png \
+  public/favicon.ico
