7b727cb4a946687a5f12da7804af088bea2ad04e
Nils Knappmeier <npm@knappi.org>
2021-07-31 14:15:33 +0200

    fix eslint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import "src/assets/styles/styles.scss"
-import {useTranslation} from "react-i18next";
 import {DefaultLayout} from "./layout/DefaultLayout";
 import i18n from "i18next";
 
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { de } from "./translations/de";
 
-export async function initI18Next() {
+export async function initI18Next(): Promise<void> {
   await i18n.use(initReactI18next).init({
     resources: {
       de: {
