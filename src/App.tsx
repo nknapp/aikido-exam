a002ae51a89ec524a16097002ef4a4ebf16a25ae
Nils Knappmeier <npm@knappi.org>
2021-07-31 14:24:17 +0200

    fix eslint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import "src/assets/styles/styles.scss";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import i18n from "i18next";
