aefd6c19c6d2c61f5fcf8413799111cc73273c24
Nils Knappmeier <npm@knappi.org>
2021-07-31 21:54:40 +0200

    fix test

--- src/utils/resolve-exam-tables.test.ts
+++ src/utils/resolve-exam-tables.test.ts
@@ -1,16 +1,44 @@
 import { resolveExamTables } from "./resolve-exam-tables";
-import { kyu5 } from "../exam-tables/kyu5";
 
 describe("resolve-exam-tables", () => {
   it("creates a list of queries", () => {
-    expect(resolveExamTables([kyu5])).toEqual([
+    const actual = resolveExamTables([
+      {
+        techniques: {
+          "suwari waza": {
+            "ryote dori": {
+              "kokyu ho": [],
+            },
+          },
+          "tachi waza": {
+            "ai hanmi katate dori": {
+              "shiho nage": ["omote", "ura"],
+              "irimi nage": [],
+            },
+          },
+        },
+      },
+      {
+        techniques: {
+          "tachi waza": {
+            "ai hanmi katate dori": {
+              ikkyo: ["omote", "ura"],
+            },
+          },
+        },
+      },
+    ]);
+    [
       ["suwari waza", "ryote dori", "kokyu ho"],
       ["tachi waza", "ai hanmi katate dori", "ikkyo", "omote"],
       ["tachi waza", "ai hanmi katate dori", "ikkyo", "ura"],
       ["tachi waza", "ai hanmi katate dori", "shiho nage", "omote"],
       ["tachi waza", "ai hanmi katate dori", "shiho nage", "ura"],
       ["tachi waza", "ai hanmi katate dori", "irimi nage"],
-    ]);
+    ].forEach((query) => {
+      expect(actual).toContainEqual(query);
+    });
+    expect(actual).toHaveLength(6);
   });
 
   it("Returns an empty list of no tables are selected", () => {
