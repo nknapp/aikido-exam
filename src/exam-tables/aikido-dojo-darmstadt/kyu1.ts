5d91855fdb398bb38e4e25dc46e078c170ea7b6a
Nils Knappmeier <npm@knappi.org>
2023-02-26 15:53:04 +0100

    add 1th kyu darmstadt

---
+++ src/exam-tables/aikido-dojo-darmstadt/kyu1.ts
@@ -0,0 +1,94 @@
+import { ExamTable } from "src/exam-tables/baseTypes";
+
+export const kyu1: ExamTable = {
+  techniques: {
+    "suwari waza": {
+      "yokomen uchi": {
+        "kote gaeshi": [],
+      },
+      "shomen uchi": {
+        gokyo: [],
+      },
+    },
+    "hanmi handachi waza": {
+      "shomen uchi": {
+        ikkyo: ["omote", "ura"],
+        nikyo: ["omote", "ura"],
+        gokyo: [],
+      },
+      "gyuako hanmi katate dori": {
+        ikkyo: ["omote", "ura"],
+        nikyo: ["omote", "ura"],
+      },
+      "ushiro ryo kata dori": {
+        ikkyo: ["omote", "ura"],
+        nikyo: ["omote", "ura"],
+        sankyo: ["omote", "ura"],
+        yonkyo: ["omote", "ura"],
+      },
+    },
+    "tachi waza": {
+      "ushiro eri dori": {
+        ikkyo: ["omote", "ura"],
+        "kote gaeshi": [],
+        "shiho nage": [],
+      },
+      "jodan tsuki": {
+        ikkyo: ["omote", "ura"],
+        nikyo: ["omote", "ura"],
+        sankyo: ["omote", "ura"],
+        yonkyo: ["omote", "ura"],
+        "shiho nage": ["omote", "ura"],
+        "irimi nage": [],
+        "soto kaiten nage": ["omote", "ura"],
+        "uchi kaiten nage": ["omote", "ura"],
+        "koshi nage": [],
+      },
+      "chudan tsuki": {
+        nikyo: ["omote", "ura"],
+        sankyo: ["omote", "ura"],
+        yonkyo: ["omote", "ura"],
+        "kote gaeshi": [],
+        "soto kaiten nage": ["omote", "ura"],
+        "uchi kaiten nage": ["omote", "ura"],
+        "koshi nage": [],
+      },
+      "shomen uchi": {
+        gokyo: [],
+        "koshi nage": [],
+      },
+      "muna dori": {
+        "shiho nage": [],
+      },
+      "ushiro ryo hiji dori": {
+        "shiho nage": ["omote", "ura"],
+      },
+      "ushiro katate dori kubi shime": {
+        ikkyo: ["omote", "ura"],
+        "kote gaeshi": [],
+        "shiho nage": [],
+      },
+      "katate ryote dori": {
+        "juji garami": [],
+      },
+      "ryote dori": {
+        "koshi nage": [],
+      },
+      "kata dori men uchi": {
+        "koshi nage": [],
+      },
+      "yokomen uchi": {
+        "koshi nage": [],
+      },
+      "kata dori": {
+        "koshi nage": [],
+      },
+      "ushiro ryote dori": {
+        "koshi nage": [],
+      },
+      "ushiro ryo kata dori": {
+        "koshi nage": [],
+      },
+    },
+  },
+};
