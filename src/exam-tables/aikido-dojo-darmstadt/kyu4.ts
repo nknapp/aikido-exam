57a1f2cede4211a92cc5f66af14641e2eef9451a
Nils Knappmeier <npm@knappi.org>
2023-02-26 11:17:21 +0100

    add 4th kyu darmstadt

---
+++ src/exam-tables/aikido-dojo-darmstadt/kyu4.ts
@@ -0,0 +1,49 @@
+import { ExamTable } from "src/exam-tables/baseTypes";
+
+export const kyu4: ExamTable = {
+  techniques: {
+    "suwari waza": {
+      "ai hanmi katate dori": {
+        ikkyo: ["omote", "ura"],
+        "irimi nage": [],
+      },
+      "shomen uchi": {
+        ikkyo: ["omote", "ura"],
+        "irimi nage": [],
+      },
+    },
+    "tachi waza": {
+      "gyuako hanmi katate dori": {
+        ikkyo: ["omote", "ura"],
+        nikyo: ["omote", "ura"],
+        "kote gaeshi": [],
+        "irimi nage": [],
+        "uchi kaiten nage": ["omote", "ura"],
+        "ude kime nage": ["omote", "ura"],
+        "sokumen irimi nage": [],
+      },
+      "kata dori": {
+        ikkyo: ["omote", "ura"],
+        nikyo: ["omote", "ura"],
+      },
+      "ai hanmi katate dori": {
+        nikyo: ["omote", "ura"],
+        "uchi kaiten sankyo": [],
+        "ude kime nage": ["omote", "ura"],
+      },
+      "shomen uchi": {
+        "kote gaeshi": [],
+      },
+      "yokomen uchi": {
+        "shiho nage": ["omote", "ura"],
+        "ude kime nage": ["omote", "ura"],
+      },
+      "ryote dori": {
+        "tenchi nage": ["omote", "ura"],
+      },
+      "ushiro ryote dori": {
+        sankyo: ["omote", "ura"],
+      },
+    },
+  },
+};
--- src/exam-tables/aikido-dojo-darmstadt/lazyData.ts
+++ src/exam-tables/aikido-dojo-darmstadt/lazyData.ts
@@ -1,11 +1,16 @@
 import { dojoLazyData } from "../baseTypes";
 import { kyu5 } from "./kyu5";
+import { kyu4 } from "./kyu4";
 
 export default dojoLazyData({
   exams: {
     kyu5: {
       labelKey: "chooser.button.kyu5",
       table: kyu5,
+    },
+    kyu4: {
+      labelKey: "chooser.button.kyu4",
+      table: kyu4,
     },
   },
   additionalText: "Diese Prüfungsordnung ist noch in Arbeit",
