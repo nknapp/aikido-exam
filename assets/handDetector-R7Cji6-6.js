83cac5a9cc3ab52582f4d878e9713f2410412ab6
nknapp <nknapp@users.noreply.github.com>
2024-02-11 00:02:45 +0000

    deploy: 2288d042fe5fb228b3c61a0a612b5dbb56546682

--- assets/handDetector-R7Cji6-6.js
+++ assets/handDetector-R7Cji6-6.js
@@ -1,4 +1,4 @@
-import{c as br,k as _0}from"./index-qopoVJdm.js";/**
+import{c as br,e as _0}from"./index-SwhZIMkO.js";/**
  * @license
  * Copyright 2020 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
