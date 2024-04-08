02f29bf5f11c153c05a70520190d8f8009aa819e
Nils Knappmeier <npm@knappi.org>
2024-04-08 21:11:36 +0200

    fix e2e-test

--- e2e-tests/TechniqueChooser.spec.ts
+++ e2e-tests/TechniqueChooser.spec.ts
@@ -2,16 +2,22 @@ import { expect, test } from "@playwright/test";
 
 test("loads keeps settings across page reload", async ({ page }) => {
   await page.goto("http://localhost:4321/en/aikido-dojo-darmstadt/");
-  await page.getByText("Random").click();
-  await expect(page.getByText("Random")).toHaveAttribute("aria-checked", "false");
+  {
+    const randomizeButton = page.getByRole("button", { name: "Random" });
+    await randomizeButton.click();
+    await expect(randomizeButton).toHaveAttribute("aria-checked", "false");
 
-  await page.getByText("5th Kyu").click();
-  await expect(page.getByText("5th Kyu")).toHaveAttribute("aria-checked", "true");
-  await expect(page.getByText("suwari waza")).toBeVisible();
-
+    const kyu5Button = page.getByRole("button", { name: "5th Kyu" });
+    await kyu5Button.click();
+    await expect(kyu5Button).toHaveAttribute("aria-checked", "true");
+    await expect(page.getByText("suwari waza")).toBeVisible();
+  }
   await page.reload();
-
-  await expect(page.getByText("Random")).toHaveAttribute("aria-checked", "false");
-  await expect(page.getByText("5th Kyu")).toHaveAttribute("aria-checked", "true");
-  await expect(page.getByText("suwari waza")).toBeVisible();
+  {
+    const randomizeButton = page.getByRole("button", { name: "Random" });
+    const kyu5Button = page.getByRole("button", { name: "5th Kyu" });
+    await expect(randomizeButton).toHaveAttribute("aria-checked", "false");
+    await expect(kyu5Button).toHaveAttribute("aria-checked", "true");
+    await expect(page.getByText("suwari waza")).toBeVisible();
+  }
 });
