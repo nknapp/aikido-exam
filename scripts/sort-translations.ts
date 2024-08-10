c72dcadf86e4e1b742bfc7227f11ec81dbad0e9f
Nils Knappmeier <npm@knappi.org>
2024-08-10 06:46:28 +0200

    sort translations

---
+++ scripts/sort-translations.ts
@@ -0,0 +1,13 @@
+import fs from "node:fs/promises";
+import prettier from "prettier";
+
+const file = process.argv[2];
+
+const translations = JSON.parse(await fs.readFile(file, "utf-8"));
+const sortedTranslations = Object.fromEntries(
+  Object.entries(translations).toSorted((a, b) => {
+    return a[0].localeCompare(b[0]);
+  }),
+);
+
+await fs.writeFile(file, await prettier.format(JSON.stringify(sortedTranslations, null, 2), { parser: "json" }));
